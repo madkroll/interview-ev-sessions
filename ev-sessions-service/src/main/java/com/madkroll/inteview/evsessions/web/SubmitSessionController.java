4ddb6a6962be17ddc3a6faad270c3021f2030f7b
Maxim Nikitin <mighty.boosta@gmail.com>
2019-06-22 16:06:12 +0200

    Moved service to sub-module. Fixed API Request: stationId is now a field of request body (JSON).

--- ev-sessions-service/src/main/java/com/madkroll/inteview/evsessions/web/SubmitSessionController.java
+++ ev-sessions-service/src/main/java/com/madkroll/inteview/evsessions/web/SubmitSessionController.java
@@ -5,8 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +21,9 @@ public class SubmitSessionController {
 
     @RequestMapping("/chargingSessions")
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<SubmitSessionResponse> submit(@RequestParam("stationId")final String stationId) {
+    public ResponseEntity<SubmitSessionResponse> submit(@RequestBody final SubmitSessionRequest submitSessionRequest) {
         return ResponseEntity
                 .ok()
-                .body(sessionService.submit(stationId));
+                .body(sessionService.submit(submitSessionRequest.getStationId()));
     }
 }
---
+++ ev-sessions-service/src/main/java/com/madkroll/inteview/evsessions/web/SubmitSessionRequest.java
@@ -0,0 +1,12 @@
+package com.madkroll.inteview.evsessions.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SubmitSessionRequest {
+
+    private final String stationId;
+
+}
